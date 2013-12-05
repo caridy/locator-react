52ea976900d1fb2da48967134218b72449cea94f
Caridy Patino <caridy@gmail.com>
2013-12-05 11:42:56 -0500

    update plugin to work with express-yui@1.x

--- yui-modules/template-react.js
+++ yui-modules/template-react.js
@@ -3,20 +3,28 @@ YUI.add('template-react', function (Y) {
     var React = Y.config.global.React;
 
     React.revive = React.revive || function (fn) {
-        return function (data, node) {
-            var fragment = !node && document.createDocumentFragment(),
-                component = React.createClass({
+        return function () {
+            var component = React.createClass({
                     render: fn
                 }),
                 instance = component();
-
-            // supporting node and elements
-            node = (node && node._node) || node;
-            // mixing in the template data
-            Y.mix(instance.props, data, true);
-            React.renderComponent(instance, node || fragment);
-            return !node && fragment.innerHTML;
+            return function (data, node) {
+                var fragment = !node && document.createDocumentFragment();
+                // supporting node and elements
+                node = (node && node._node) || node;
+                // mixing in the template data
+                Y.mix(instance.props, data, true);
+                if (node) {
+                    React.renderComponent(instance, node);
+                    return; // not need to return the html since the node was passed in
+                }
+                // fallback to return the html if node was not provided
+                React.renderComponent(instance, fragment);
+                return fragment.innerHTML;
+            };
         };
     };
+
+    Y.React = React;
 
 }, '', {requires: ['template-base']});
